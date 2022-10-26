bc9380e961338ef53656fa16ee7643b906ae0ac5
Jeff <jeydt23@gmail.com>
2022-10-25 23:42:33 -0400

    Get SPOT details from id and CREATE a spot done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
     res.json({ Spots });
 })
 
-// GET ALL SPOTS OWNED BY CURRENT OWNER
+// GET ALL SPOTS OWNED BY CURRENT USER
 router.get('/current', requireAuth, async (req, res) => {
     const allSpots = await Spot.findAll({
         where: {
@@ -112,15 +112,24 @@ router.get('/current', requireAuth, async (req, res) => {
 // GET DETAILS OF A SPOT FROM AN ID
 
 router.get('/:spotId', async (req, res) => {
-    // const { spotId } = req.params;
+    // const { spotId } = req.params DOESN'T WORK, WHY????
     const detailsOfSpot = await Spot.findByPk(req.user.id, {
-        include: {
-            model: User, as: "Owner",
-            attributes: ['id', 'firstName', 'lastName']
-        }
+        include: [
+            { model: Review, attributes: [] },
+            { model: SpotImage, as: "SpotImages", attributes: ['id', 'url', 'preview'] },
+            { model: User, as: "Owner", attributes: ['id', 'firstName', 'lastName'] },
+        ]
     });
 
-    if (!detailsOfSpot){
+    const countReview = await Review.count({ where: { spotId: req.user.id } });
+    const sumStar = await Review.sum('stars', { where: { spotId: req.user.id } });
+
+    let average = sumStar / countReview;
+    detailsOfSpot.dataValues.numReviews = countReview;
+
+    detailsOfSpot.dataValues.avgStarRating = average;
+
+    if (!detailsOfSpot) {
         res.status(404);
         res.json({
             message: "Spot couldn't be found",
@@ -128,7 +137,41 @@ router.get('/:spotId', async (req, res) => {
         })
     }
 
+    res.json(detailsOfSpot)
 
 });
+
+
+// CREATE A SPOT
+
+router.post('/', requireAuth, async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+    try {
+        const createSpot = await Spot.create({
+            ownerId: req.user.id,
+            address, city, state, country, lat, lng, name, description, price
+        })
+        res.json(createSpot);
+
+    } catch {
+        res.status(400);
+        res.json({
+            "message": "Validation Error",
+            "statusCode": 400,
+            "errors": {
+                "address": "Street address is required",
+                "city": "City is required",
+                "state": "State is required",
+                "country": "Country is required",
+                "lat": "Latitude is not valid",
+                "lng": "Longitude is not valid",
+                "name": "Name must be less than 50 characters",
+                "description": "Description is required",
+                "price": "Price per day is required"
+            }
+        })
+    }
+})
 
 module.exports = router;
