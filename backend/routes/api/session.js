4b30d6292f74a7dbb830709544edd72b93b2182f
Jeff <jeydt23@gmail.com>
2022-10-28 16:20:38 -0400

    session.js fixed

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,11 +42,15 @@ router.post(
     const user = await User.login({ credential, password });
 
     if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
+      // const err = new Error('Login failed');
+      // err.status = 401;
+      // err.title = 'Login failed';
+      // err.errors = 'Invalid credentials';
+      // return next(err);
+      return res.status(401).json({
+        message: 'Invalid credentials',
+        statusCode: 401
+      })
     }
 
     const token = await setTokenCookie(res, user);
