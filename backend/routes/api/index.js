3440c7cc60674ffb79c87d8c59442a6ccd7027d3
Jeff <jeydt23@gmail.com>
2022-11-11 16:25:32 -0500

    ready for heroku

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,80 +1,36 @@
-// // backend/routes/api/index.js
-// const router = require('express').Router();
-// const sessionRouter = require('./session.js');
-// const usersRouter = require('./users.js');
-// // const spotsRouter = require('./spots.js')
-// const { restoreUser } = require("../../utils/auth.js");
-// const spotsRouter = require("./spots.js")
-// const reviewsRouter = require("./reviews.js")
-// const bookingsRouter = require("./bookings.js")
-// const spotImageRouter = require('./spot-images.js')
-// const reviewImageRouter = require('./review-images.js')
-// // Connect restoreUser middleware to the API router
-//   // If current user session is valid, set req.user to the user in the database
-//   // If current user session is not valid, set req.user to null
-// router.use(restoreUser);
+// backend/routes/api/index.js
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// const spotsRouter = require('./spots.js')
+const { restoreUser } = require("../../utils/auth.js");
+const spotsRouter = require("./spots.js")
+const reviewsRouter = require("./reviews.js")
+const bookingsRouter = require("./bookings.js")
+const spotImageRouter = require('./spot-images.js')
+const reviewImageRouter = require('./review-images.js')
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
 
-// router.use('/session', sessionRouter);
+router.use('/session', sessionRouter);
 
-// router.use('/users', usersRouter);
+router.use('/users', usersRouter);
 
-// router.use('/spots', spotsRouter);
+router.use('/spots', spotsRouter);
 
-// router.use('/reviews', reviewsRouter);
+router.use('/reviews', reviewsRouter);
 
-// router.use('/bookings', bookingsRouter);
+router.use('/bookings', bookingsRouter);
 
-// router.use('/spot-images', spotImageRouter);
+router.use('/spot-images', spotImageRouter);
 
-// router.use('/review-images', reviewImageRouter);
+router.use('/review-images', reviewImageRouter);
 
-// router.post('/test', (req, res) => {
+router.post('/test', (req, res) => {
 
-//   res.json({ requestBody: req.body });
-// });
-
-// module.exports = router;
-
-
-
-
-// backend/routes/index.js
-const express = require('express');
-const router = express.Router();
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
-
-// Static routes
-// Serve React build files in production
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  // Serve the frontend's index.html file at the root route
-  router.get('/', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-    );
-  });
-
-  // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../frontend/build")));
-
-  // Serve the frontend's index.html file at all other routes NOT starting with /api
-  router.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-    );
-  });
-}
-
-// Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.status(201).json({});
-  });
-}
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
